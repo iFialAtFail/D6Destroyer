2687913bdc793076e802d38ab22fd0d0b8d12166
Michael Manley <mmanle01@gmail.com>
2018-04-15 21:23:41 -0400

    Added firebase Asset/api and started working on the gui.

---
+++ Assets/InitializeFirebase.cs
@@ -0,0 +1,15 @@
+﻿using Firebase;
+using Firebase.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeFirebase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Set this before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://d6-destroyer.firebaseio.com/");
+	}
+	
+}
